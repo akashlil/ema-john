a342829828c72e26c1800da4b168a9279173636c
akash <akashlil393@gmail.com>
2021-10-03 15:19:15 +0600

    shopping finsh

--- src/components/Addtocard/Buycard.js
+++ src/components/Addtocard/Buycard.js
@@ -1,36 +1,43 @@
-import React from 'react';
-import "./Buycard.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Buycard.css";
 
 const Buycard = (props) => {
-    let price = 0;
-    let Qunitity = 0;
-    for (const allproduct of props.cartAdd) {
-       const  productQunitity = !allproduct.qunitity ? 1 : allproduct.qunitity;
-        price =( price + allproduct.price) * productQunitity;
-        Qunitity = Qunitity + productQunitity;
-    }
+  let price = 0;
+  let Qunitity = 0;
+  for (const allproduct of props.cartAdd) {
+    const productQunitity = !allproduct.qunitity ? 1 : allproduct.qunitity;
+    price = (price + allproduct.price) * productQunitity;
+    Qunitity = Qunitity + productQunitity;
+  }
 
-    let shipping = 50;
-    if ( price > 200 ) {
-        shipping=100
-    } 
-    const tax = ((price / 100) * 10);
-    const total = price + shipping + tax;
-    return (
-        <div className="add-to-cart">
-            <div>
-            <h1>Order Summary</h1>
-                <p>Items ordered: {Qunitity}</p>
-            <ul>
-                    <li>Items : $ {price.toFixed(2) }</li>
-                    <li>Shipping & Handling : $ { shipping.toFixed(2)}</li>
-                    <li>Total  tax : $ { tax.toFixed(2)}</li>
-                    <li>Order Total : $ { total.toFixed(2)}</li>
-            </ul>
+  let shipping = 50;
+  if (price > 200) {
+    shipping = 100;
+  }
+  const tax = (price / 100) * 10;
+  const total = price + shipping + tax;
+  return (
+    <div className="add-to-cart">
+      <div>
+        <h1>Order Summary</h1>
+        <p>Items ordered: {Qunitity}</p>
+        <ul>
+          <li>Items : $ {price.toFixed(2)}</li>
+          <li>Shipping & Handling : $ {shipping.toFixed(2)}</li>
+          <li>Total tax : $ {tax.toFixed(2)}</li>
+          <li>Order Total : $ {total.toFixed(2)}</li>
+        </ul>
+        {props.children ? (
+          props.children
+        ) : (
+          <Link to="/order">
             <button className="card-btn">Review your order</button>
-         </div>
-        </div>
-    );
+          </Link>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Buycard;
--- src/components/OderReview/OderReview.js
+++ src/components/OderReview/OderReview.js
@@ -1,25 +1,42 @@
 import Buycard from "../Addtocard/Buycard";
+import { deleteitem, removeItemAll } from "../FackDB/FackBd";
 import useCart from "../Hock/uesCart";
 import useLoadData from "../Hock/useLoadData";
 import Product from "../Products/Product";
 
 const OderReview = () => {
   const [datas] = useLoadData();
-  const [cartAdd] = useCart(datas);
+  const [cartAdd, setCartAdd] = useCart(datas);
   console.log(cartAdd);
 
+  const removeItem = (id) => {
+    deleteitem(id);
+    const cardUpadte = cartAdd.filter((cd) => cd.key !== id);
+    setCartAdd(cardUpadte);
+  };
+
+  const shopingFinsh = () => {
+    setCartAdd([]);
+    removeItemAll();
+  };
   return (
     <div className="conteaner">
       <div className="product-data">
         {cartAdd.map((cd) => (
           <Product product={cd}>
-            <button className="card-btn">Remove Item</button>
+            <button onClick={() => removeItem(cd.key)} className="card-btn">
+              Remove Item
+            </button>
           </Product>
         ))}
         {/* <Product product={cartAdd}></Product> */}
       </div>
       <div>
-        <Buycard cartAdd={cartAdd}></Buycard>
+        <Buycard cartAdd={cartAdd}>
+          <button onClick={shopingFinsh} className="card-btn">
+            shopingFinsh
+          </button>
+        </Buycard>
       </div>
     </div>
   );
